aa2497a3ad89e3be125d7abc3c307547c4b3ba7f
mrLuisFer <mrluisfeer@gmail.com>
2021-11-16 11:50:06 -0600

    create a clickPopup component for show text when click in some element

---
+++ components/common/ClickPopup/index.js
@@ -0,0 +1,41 @@
+import { useState } from 'react'
+import { Box, Text, ScaleFade } from '@chakra-ui/react'
+import { gray } from 'lib/chakraColors'
+
+export default function ClickPopup({ text = '', children, bgColor = gray.gray700, width, height }) {
+  const [showPopup, setShowPopup] = useState(false)
+
+  const handleShowPopup = () => {
+    setShowPopup((prevState) => !prevState)
+
+    setTimeout(() => {
+      setShowPopup(false)
+    }, 2000)
+  }
+
+  return (
+    <Box onClick={handleShowPopup} position="relative">
+      {children}
+      {showPopup && (
+        <Box
+          position="absolute"
+          bg={bgColor}
+          p="0.5rem"
+          borderRadius="10px"
+          mt="0.5rem"
+          zIndex="10"
+          w={width}
+          h={height}
+        >
+          <ScaleFade initialScale={0.85} in={showPopup}>
+            <Box>
+              <Text fontSize="xs" color="white" textAlign="center">
+                {text}
+              </Text>
+            </Box>
+          </ScaleFade>
+        </Box>
+      )}
+    </Box>
+  )
+}
--- components/home/MetaForm/formComponents/InputLabel.js
+++ components/home/MetaForm/formComponents/InputLabel.js
@@ -1,18 +1,34 @@
-import { FormLabel } from '@chakra-ui/react'
-import { teal } from 'lib/chakraColors'
+import { FormLabel, Flex, Button } from '@chakra-ui/react'
+import { IoMdInformationCircleOutline } from 'react-icons/io'
+import { teal, gray } from 'lib/chakraColors'
+import ClickPopup from 'components/common/ClickPopup'
 
 export default function InputLabel({ children, isFocus, inputId, ...props }) {
   return (
-    <FormLabel
-      _hover={{ color: teal.teal100 }}
-      color={isFocus ? teal.teal200 : ''}
-      display="inline-block"
-      fontWeight="semibold"
-      htmlFor={inputId}
-      userSelect="none"
-      {...props}
-    >
-      {children}
-    </FormLabel>
+    <Flex align="center" justify="space-between">
+      <FormLabel
+        _hover={{ color: teal.teal100 }}
+        color={isFocus ? teal.teal200 : ''}
+        display="inline-block"
+        fontWeight="semibold"
+        htmlFor={inputId}
+        userSelect="none"
+        {...props}
+      >
+        {children}
+      </FormLabel>
+      <ClickPopup text="Info">
+        <Button
+          _hover={{ background: gray.gray700 }}
+          p="0rem"
+          m="0.4rem"
+          size="sm"
+          title="Click to know what's this attribute"
+          bg="transparent"
+        >
+          <IoMdInformationCircleOutline size="1.5em" />
+        </Button>
+      </ClickPopup>
+    </Flex>
   )
 }
