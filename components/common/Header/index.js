c74365c9b6d8e6f04bb4135ceff1582d35bbeb1b
mrLuisFer <mrluisfeer@gmail.com>
2021-11-29 09:00:22 -0600

    add Link for return to home when click the logo

--- components/common/Header/index.js
+++ components/common/Header/index.js
@@ -1,20 +1,23 @@
+import Link from 'next/link'
 import { Flex, Heading, Text, HStack } from '@chakra-ui/react'
 import { SiSpringCreators } from 'react-icons/si'
-import { teal } from 'lib/chakraColors'
+import { teal, white } from 'lib/chakraColors'
 import HoverPopup from '../HoverPopup'
 import MenuIcon from './MenuIcon'
 
 export default function Header() {
   return (
     <HStack as="header" align="center" mb="1rem" justifyContent="space-between">
-      <Flex gridGap="0.5rem" alignItems="center">
+      <Flex gridGap="0.5rem" alignItems="center" cursor="pointer">
         <HoverPopup text="Made by mrLuisFer" width="8rem">
           <Text color={teal.teal300}>
             <SiSpringCreators size="2rem" />
           </Text>
         </HoverPopup>
-        <Heading as="h1" size="md">
-          SEO Generator
+        <Heading as="h1" size="md" color={white} userSelect="none">
+          <Link href="/" key="home">
+            SEO Generator
+          </Link>
         </Heading>
       </Flex>
       <MenuIcon />
