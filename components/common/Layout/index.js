f2a1c9c3b6377229643bce51ec38448f6aa3da94
mrLuisFer <mrluisfeer@gmail.com>
2021-12-01 12:36:29 -0600

    change static color for a js color variable

--- components/common/Layout/index.js
+++ components/common/Layout/index.js
@@ -1,10 +1,11 @@
 import { Container } from '@chakra-ui/react'
 import Header from '../Header'
 import MenuMobile from '../../MenuMobile'
+import { gray } from 'lib/chakraColors'
 
 export default function Layout({ children }) {
   return (
-    <Container bg="gray.800" p="2rem 3rem" minH="100vh" minW="100vw" m="0" maxW="container.lg">
+    <Container bg={gray.gray800} p="2rem 3rem" minH="100vh" minW="100vw" m="0" maxW="container.lg">
       <Header />
       {children}
       <MenuMobile />
