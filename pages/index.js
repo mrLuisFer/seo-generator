23e4703b32691a3e9218a416acc64560eef287c4
mrLuisFer <mrluisfeer@gmail.com>
2021-12-17 13:29:14 -0600

    fix import path

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { Box } from '@chakra-ui/react'
 import SEO from 'components/common/SEO'
-import Home from './components/home'
+import Home from 'components/home'
 
 export default function Main() {
 
