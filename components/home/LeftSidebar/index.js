b69e3c71714e84a025fc9a96e14dd6f4197e2954
mrLuisFer <mrluisfeer@gmail.com>
2021-12-17 13:29:37 -0600

    move inputs state into home

--- components/home/LeftSidebar/index.js
+++ components/home/LeftSidebar/index.js
@@ -1,27 +1,11 @@
-import { useState } from 'react'
 import MetaForm from "./MetaForm"
 
-const requiredInputsInitialState = {
-  descriptionTextarea: '',
-  imgInput: '',
-  keywordsInput: '',
-  ogTypeInput: '',
-  titleInput: '',
-}
-
-const optionalInputsInitialState = {
-  author: '',
-  copyright: '',
-  ogUrl: '',
-  rating: '',
-  robots: '',
-}
-
-
-export default function LeftSidebar() {
-  const [requiredInputValues, setRequiredInputValues] = useState(requiredInputsInitialState)
-  const [optionalInputValues, setOptionalInputValues] = useState(optionalInputsInitialState)
-
+export default function LeftSidebar({
+  optionalInputValues,
+  requiredInputValues,
+  setOptionalInputValues,
+  setRequiredInputValues,
+}) {
 
   return (
     <div>
--- components/home/index.js
+++ components/home/index.js
@@ -1,12 +1,36 @@
-import { Box } from '@chakra-ui/react'
+import { useState } from 'react'
+import { Flex } from '@chakra-ui/react'
 import LeftSidebar from "./LeftSidebar"
 import RightSidebar from './RightSidebar'
 
+const requiredInputsInitialState = {
+  descriptionTextarea: '',
+  imgInput: '',
+  keywordsInput: '',
+  ogTypeInput: '',
+  titleInput: '',
+}
+
+const optionalInputsInitialState = {
+  author: '',
+  copyright: '',
+  ogUrl: '',
+  rating: '',
+  robots: '',
+}
+
 export default function Home() {
+  const [requiredInputValues, setRequiredInputValues] = useState(requiredInputsInitialState)
+  const [optionalInputValues, setOptionalInputValues] = useState(optionalInputsInitialState)
+
   return (
-    <Box>
-      <LeftSidebar />
+    <Flex>
+      <LeftSidebar
+        optionalInputValues={optionalInputValues}
+        requiredInputValues={requiredInputValues}
+        setOptionalInputValues={setOptionalInputValues}
+        setRequiredInputValues={setRequiredInputValues} />
       <RightSidebar />
-    </Box>
+    </Flex>
   )
 }
