fa938f4c753270d9fa766a47b6b4274142ad4c2d
mrLuisFer <mrluisfeer@gmail.com>
2021-12-18 00:34:21 -0600

    remove styles for try to fix

--- components/common/Layout/index.js
+++ components/common/Layout/index.js
@@ -1,11 +1,10 @@
 import { Container } from '@chakra-ui/react'
 import Header from '../Header'
 import MenuMobile from '../../MenuMobile'
-import { gray } from 'lib/chakraColors'
 
 export default function Layout({ children }) {
   return (
-    <Container bg={gray.gray800} p="2rem 3rem" minH="100vh" minW="100vw" m="0" maxW="container.lg">
+    <Container minW="100vw" p="1rem 2rem">
       <Header />
       {children}
       <MenuMobile />
