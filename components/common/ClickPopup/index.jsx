32d7588d02d3c85b50bc0adae213e8291aa3eb0f
mrLuisFer <mrluisfeer@gmail.com>
2021-12-20 17:42:28 -0600

    add event when click outside the element

--- components/common/ClickPopup/index.jsx
+++ components/common/ClickPopup/index.jsx
@@ -1,17 +1,27 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { Box, Text, ScaleFade } from '@chakra-ui/react'
-import { gray } from 'lib/chakraColors'
+import { gray, white } from 'lib/chakraColors'
 
-export default function ClickPopup({ text = '', children, bgColor = gray.gray700, width, height }) {
+export default function ClickPopup({ text = '', children, bgColor = gray.gray700, textColor = white, width, height }) {
   const [showPopup, setShowPopup] = useState(false)
 
   const handleShowPopup = () => {
-    setShowPopup((prevState) => !prevState)
-
-    setTimeout(() => {
-      setShowPopup(false)
-    }, 2000)
+    setShowPopup(true)
   }
+
+  const ref = useRef(null)
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setShowPopup(false)
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true)
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true)
+    }
+  })
 
   return (
     <Box onClick={handleShowPopup} position="relative">
@@ -20,12 +30,15 @@ export default function ClickPopup({ text = '', children, bgColor = gray.gray700
         <Box
           position="absolute"
           bg={bgColor}
+          color={textColor}
           p="0.5rem"
           borderRadius="10px"
           mt="0.5rem"
           zIndex="10"
           w={width}
           h={height}
+          ref={ref}
+          userSelect="none"
         >
           <ScaleFade initialScale={0.85} in={showPopup}>
             <Box>
