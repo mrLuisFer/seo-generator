987bbd13bd20a85c9314e4ae7d6e41f02196324b
mrLuisFer <mrluisfeer@gmail.com>
2021-12-20 17:42:55 -0600

    make contributors component for the future

---
+++ components/common/Header/Contributors.jsx
@@ -0,0 +1,22 @@
+import { Box, Text } from '@chakra-ui/react'
+import { BsFillPeopleFill } from 'react-icons/bs'
+import ClickPopup from '../ClickPopup'
+import { blue } from 'lib/chakraColors'
+
+export default function Contributors() {
+	return (
+		<Box
+			border="2px"
+			p="4px"
+			borderRadius="10px"
+			borderColor="transparent"
+			_hover={{ borderColor: blue.blue100 }}
+		>
+			<ClickPopup text="Made by mrLuisFer" width="8rem">
+				<Text cursor="pointer">
+					<BsFillPeopleFill size="1.6rem" />
+				</Text>
+			</ClickPopup>
+		</Box>
+	)
+}
