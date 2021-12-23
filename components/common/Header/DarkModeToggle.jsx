3c97b6fedce79d097e80f0e2112f87dd9a6818c2
mrLuisFer <mrluisfeer@gmail.com>
2021-12-22 22:21:03 -0600

    add details for dark mode toggle

--- components/common/Header/DarkModeToggle.jsx
+++ components/common/Header/DarkModeToggle.jsx
@@ -2,13 +2,29 @@ import { useColorMode } from '@chakra-ui/react'
 import HeaderBtn from './HeaderBtn'
 import { BsFillMoonFill } from 'react-icons/bs'
 import { FaRegSun } from 'react-icons/fa'
+import HoverPopup from '../HoverPopup'
+import { gray } from 'lib/chakraColors'
+import { useDarkMode } from 'hooks/useDarkMode'
 
 export default function DarkModeToggle() {
-  const { colorMode, toggleColorMode } = useColorMode()
+	const { colorMode, toggleColorMode } = useColorMode()
+	const { isDarkMode } = useDarkMode()
+	const txtMode = isDarkMode ? 'dark' : 'light'
 
-  return (
-    <HeaderBtn onClick={toggleColorMode} display="flex" justifyContent="center" alignItems="center">
-      {colorMode === 'light' ? <BsFillMoonFill size="1.2rem" /> : <FaRegSun size="1.2rem" />}
-    </HeaderBtn>
-  )
+	return (
+		<HoverPopup
+			text={`Change ${txtMode} mode`}
+			width="130px"
+			bgColor={isDarkMode ? gray.$300 : gray.$600}
+		>
+			<HeaderBtn
+				onClick={toggleColorMode}
+				display="flex"
+				justifyContent="center"
+				alignItems="center"
+			>
+				{colorMode === 'light' ? <BsFillMoonFill size="1.2rem" /> : <FaRegSun size="1.2rem" />}
+			</HeaderBtn>
+		</HoverPopup>
+	)
 }
