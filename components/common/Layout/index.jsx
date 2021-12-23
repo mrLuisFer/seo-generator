c1474d55ea0249bfbb97cb8454206e7bb965aace
mrLuisFer <mrluisfeer@gmail.com>
2021-12-22 19:39:28 -0600

    add hook for dark mode or light mode value

--- components/common/Layout/index.jsx
+++ components/common/Layout/index.jsx
@@ -1,11 +1,13 @@
-import { Container } from '@chakra-ui/react'
+import { Container, useColorModeValue } from '@chakra-ui/react'
 import Header from '../Header'
 import MenuMobile from '../../MenuMobile'
-import { gray } from 'lib/chakraColors'
+import { gray, white } from 'lib/chakraColors'
 
 export default function Layout({ children }) {
+	const textColor = useColorModeValue(gray.$900, white)
+
   return (
-    <Container maxW="container.xl" p="1rem 2rem" color={gray.$900}>
+    <Container maxW="container.xl" p="1rem 2rem" color={textColor}>
       <Header />
       {children}
       <MenuMobile />
