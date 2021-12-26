31bde482db6b2f2973703c7232592d6fbf0088b7
mrLuisFer <mrluisfeer@gmail.com>
2021-12-25 19:31:56 -0600

    add some styles and create dropDownItem component

--- components/common/Header/Logo/DropDownMenuToggle/DropDownMenu.jsx
+++ components/common/Header/Logo/DropDownMenuToggle/DropDownMenu.jsx
@@ -1,7 +1,22 @@
+import { Box } from '@chakra-ui/react'
+import { gray, white } from 'lib/chakraColors'
+
+function DropDownItem({ children }) {
+  return <Box>{children}</Box>
+}
+
 export default function DropDownMenu() {
-	return (
-		<div>
-			<p>Some</p>
-		</div>
-	)
+  return (
+    <Box
+      position="absolute"
+      bg={gray.$700}
+      borderRadius="5px"
+      color={white}
+      marginTop="0.5rem"
+      p="2rem 0"
+      zIndex="10"
+    >
+      <DropDownItem>item</DropDownItem>
+    </Box>
+  )
 }
