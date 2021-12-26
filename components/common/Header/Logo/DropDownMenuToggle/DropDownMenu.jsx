8c1457f8939e009edd2c8cbde4e85e7ae2337158
mrLuisFer <mrluisfeer@gmail.com>
2021-12-25 20:22:39 -0600

    add details to drop down item

--- components/common/Header/Logo/DropDownMenuToggle/DropDownMenu.jsx
+++ components/common/Header/Logo/DropDownMenuToggle/DropDownMenu.jsx
@@ -1,22 +1,37 @@
-import { Box } from '@chakra-ui/react'
+import { VStack, Text } from '@chakra-ui/react'
 import { gray, white } from 'lib/chakraColors'
+import { transitionProperty } from 'lib/transitionProperty'
 
 function DropDownItem({ children }) {
-  return <Box>{children}</Box>
+  return (
+    <Text
+      _hover={{ background: gray.$600 }}
+      fontWeight="semibold"
+      cursor="pointer"
+      p="0.5rem 3rem"
+      userSelect="none"
+      transition={transitionProperty('background')}
+    >
+      {children}
+    </Text>
+  )
 }
 
 export default function DropDownMenu() {
   return (
-    <Box
+    <VStack
       position="absolute"
       bg={gray.$700}
       borderRadius="5px"
       color={white}
       marginTop="0.5rem"
-      p="2rem 0"
+      p="0.5rem 0"
       zIndex="10"
+      spacing="0"
     >
       <DropDownItem>item</DropDownItem>
-    </Box>
+      <DropDownItem>item</DropDownItem>
+      <DropDownItem>item</DropDownItem>
+    </VStack>
   )
 }
