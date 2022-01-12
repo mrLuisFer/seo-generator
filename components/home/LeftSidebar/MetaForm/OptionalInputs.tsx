0d657d043a41a61b20f878a8a70eacd2997aa8e1
mrLuisFer <mrluisfeer@gmail.com>
2022-01-12 09:41:12 -0600

    remove spinner and loader and test useCallback for input value

--- components/home/LeftSidebar/MetaForm/OptionalInputs.tsx
+++ components/home/LeftSidebar/MetaForm/OptionalInputs.tsx
@@ -1,63 +1,55 @@
-import { useEffect, useState } from 'react'
-import { Spinner } from '@chakra-ui/spinner'
+import { useCallback } from 'react'
 import InputForm from './formComponents/InputForm'
 
 export default function OptionalMetatagsInputs({ optionalInputValues, setOptionalInputValues }) {
-  const [showSpinner, setShowSpinner] = useState(true)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSpinner(false)
-    }, 700)
-    return () => clearTimeout(timer)
-  }, [])
-
-  const handleOptInputsChange = (e) => {
-    const { name, value } = e.target
-    setOptionalInputValues({
-      ...optionalInputValues,
-      [name]: value,
-    })
-  }
+  const handleOptInputsChange = useCallback(
+    (e: any) => {
+      const { name, value } = e.target
+      setOptionalInputValues({
+        ...optionalInputValues,
+        [name]: value,
+      })
+    },
+    [optionalInputValues, setOptionalInputValues]
+  )
 
   return (
     <>
-      {showSpinner ? (
-        <Spinner size="xl" display="block" />
-      ) : (
-        <>
-          <InputForm
-            inputId="ogUrlInput"
-            labelValue="Og:Url"
-            onChange={handleOptInputsChange}
-            placeholder="https://some-page-url.com/"
-          />
-          <InputForm
-            inputId="robotsInput"
-            labelValue="Robots"
-            onChange={handleOptInputsChange}
-            placeholder="noindex, nofollow"
-          />
-          <InputForm
-            inputId="ratingInput"
-            labelValue="Rating"
-            onChange={handleOptInputsChange}
-            placeholder="adult"
-          />
-          <InputForm
-            inputId="authorInput"
-            labelValue="Author"
-            onChange={handleOptInputsChange}
-            placeholder="Author name"
-          />
-          <InputForm
-            inputId="copyrightInput"
-            labelValue="Copyright"
-            onChange={handleOptInputsChange}
-            placeholder="Copyright owner"
-          />
-        </>
-      )}
+      <InputForm
+        inputId="ogUrlInput"
+        inputType="text"
+        labelValue="Og:Url"
+        onChange={handleOptInputsChange}
+        placeholder="https://some-page-url.com/"
+      />
+      <InputForm
+        inputId="robotsInput"
+        inputType="text"
+        labelValue="Robots"
+        onChange={handleOptInputsChange}
+        placeholder="noindex, nofollow"
+      />
+      <InputForm
+        inputId="ratingInput"
+        inputType="text"
+        labelValue="Rating"
+        onChange={handleOptInputsChange}
+        placeholder="adult"
+      />
+      <InputForm
+        inputId="authorInput"
+        inputType="text"
+        labelValue="Author"
+        onChange={handleOptInputsChange}
+        placeholder="Author name"
+      />
+      <InputForm
+        inputId="copyrightInput"
+        inputType="text"
+        labelValue="Copyright"
+        onChange={handleOptInputsChange}
+        placeholder="Copyright owner"
+      />
     </>
   )
 }
