b62cec2e3914facda6d22ad9ea6d717914d01261
mrLuisFer <mrluisfeer@gmail.com>
2022-01-18 21:39:05 -0600

    add state for show middle line in the icon

--- components/home/RightSidebar/HeaderSidebar/HideTitlesBtn/HideTitlesIcon.tsx
+++ components/home/RightSidebar/HeaderSidebar/HideTitlesBtn/HideTitlesIcon.tsx
@@ -1,10 +1,10 @@
 import { MdTitle } from 'react-icons/md'
 import styles from './styles.module.css'
 
-export default function HideTitlesIcon() {
+export default function HideTitlesIcon({ showMiddleline }: { showMiddleline: boolean }) {
   return (
-    <div className={styles.hideTitlesIconContainer}>
-      <MdTitle size="1.5rem" />
+    <div className={showMiddleline ? styles.hideTitlesIconContainer : ''}>
+      <MdTitle display="inline-block" size="1.6rem" />
     </div>
   )
 }
--- components/home/RightSidebar/HeaderSidebar/HideTitlesBtn/index.tsx
+++ components/home/RightSidebar/HeaderSidebar/HideTitlesBtn/index.tsx
@@ -1,16 +1,23 @@
-import HeaderBtn from '../HeaderBtn'
+import { useState } from 'react'
 import HoverPopup from 'components/common/HoverPopup'
+import HeaderBtn from '../HeaderBtn'
 import { useSidebarTitlesCtx } from 'context/SidebarTitlesCtx'
 import HideTitlesIcon from './HideTitlesIcon'
 
 export default function HideTitlesBtn() {
   const { setShowTitles } = useSidebarTitlesCtx()
+  const [showMiddleline, setShowMiddleline] = useState<boolean>(true)
+
+  const handleShowTitles = () => {
+    setShowTitles((prevState: boolean) => !prevState)
+    setShowMiddleline((prevState: boolean) => !prevState)
+  }
 
   return (
-    <HeaderBtn onClick={() => setShowTitles((prevState: boolean) => !prevState)}>
-      <HoverPopup text="Hide Tag  titles" width="100px">
-        <HideTitlesIcon />
-      </HoverPopup>
-    </HeaderBtn>
+    <HoverPopup text="Hide Tag  titles" width="100px">
+      <HeaderBtn onClick={handleShowTitles}>
+        <HideTitlesIcon showMiddleline={showMiddleline} />
+      </HeaderBtn>
+    </HoverPopup>
   )
 }
