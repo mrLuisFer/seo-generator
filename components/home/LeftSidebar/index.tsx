fe38cea2f45ea8b7e9d833c0beabc9bb67e8a6f0
mrLuisFer <mrluisfeer@gmail.com>
2022-01-20 20:21:11 -0600

    change type for the props and add vStack component

--- components/home/LeftSidebar/index.tsx
+++ components/home/LeftSidebar/index.tsx
@@ -1,10 +1,10 @@
-import { Box } from '@chakra-ui/react'
+import { Box, VStack } from '@chakra-ui/react'
 import { Dispatch, SetStateAction } from 'react'
 import InputForm from './MetaForm/formComponents/InputForm'
 import TextareaForm from './MetaForm/formComponents/TextareaForm'
 
 interface LeftSidebarProps {
-  [x: string]: Dispatch<SetStateAction<string>>
+  [setStateFunction: string]: Dispatch<SetStateAction<string>>
 }
 
 export default function LeftSidebar({
@@ -18,46 +18,35 @@ export default function LeftSidebar({
   setRating,
   setRobots,
 }: LeftSidebarProps) {
-  console.log(
-    setKeywordsInput,
-    setOgTypeInput,
-    setAuthor,
-    setCopyright,
-    setOgUrl,
-    setRating,
-    setRobots
-  )
+  console.log(setKeywordsInput, setOgTypeInput, setAuthor, setCopyright, setOgUrl, setRating, setRobots)
 
   return (
     <Box as="section" gridColumn="span 3">
-      <InputForm
-        inputId="titleInput"
-        labelValue="Title"
-        placeholder="A awesome title"
-        setState={setTitleInput}
-        required
-      />
-      <TextareaForm
-        labelValue="Description"
-        placeholder="A nice site created with some tech stack :D"
-        required
-        resize="none"
-        setStateValue={setDescriptionTextarea}
-        textareaId="descriptionTextarea"
-      />
-      <InputForm
-        inputId="keywordsInput"
-        labelValue="Keywords"
-        placeholder="keyword 1, keyword 2, keyword 3"
-        setState={setKeywordsInput}
-        required
-      />
-      <InputForm
-        inputId="ogTypeInput"
-        setState={setOgTypeInput}
-        labelValue="Og:Type"
-        placeholder="website"
-      />
+      <VStack as="form" spacing="1.2rem">
+        <InputForm
+          inputId="titleInput"
+          labelValue="Title"
+          placeholder="A awesome title"
+          setState={setTitleInput}
+          required
+        />
+        <TextareaForm
+          labelValue="Description"
+          placeholder="A nice site created with some tech stack :D"
+          required
+          resize="none"
+          setStateValue={setDescriptionTextarea}
+          textareaId="descriptionTextarea"
+        />
+        <InputForm
+          inputId="keywordsInput"
+          labelValue="Keywords"
+          placeholder="keyword 1, keyword 2, keyword 3"
+          setState={setKeywordsInput}
+          required
+        />
+        <InputForm inputId="ogTypeInput" setState={setOgTypeInput} labelValue="Og:Type" placeholder="website" />
+      </VStack>
     </Box>
   )
 }
