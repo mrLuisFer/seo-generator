aa5dbba9e7987a9020e899413152f3f70caf404d
mrLuisFer <mrluisfeer@gmail.com>
2022-01-23 13:03:19 -0600

    add Fade component for a fancy transition

--- components/home/RightSidebar/index.tsx
+++ components/home/RightSidebar/index.tsx
@@ -1,10 +1,13 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Fade } from '@chakra-ui/react'
 import { gray } from 'utils/chakraColors'
 import MetaTags from './MetaTags'
 import HeaderSidebar from './HeaderSidebar'
 import SidebarTitlesCtxProvider from 'context/SidebarTitlesCtx'
+import { useSidebarCtx } from 'hooks/useSidebarCtx'
 
 export default function RightSidebar() {
+  const { hideSidebar } = useSidebarCtx()
+
   return (
     <SidebarTitlesCtxProvider>
       <Box
@@ -16,9 +19,12 @@ export default function RightSidebar() {
         borderRadius="15px"
         p="1.5rem"
         boxShadow="md"
+        position="relative"
       >
-        <HeaderSidebar />
-        <MetaTags />
+        <Fade in={!hideSidebar}>
+          <HeaderSidebar />
+          <MetaTags />
+        </Fade>
       </Box>
     </SidebarTitlesCtxProvider>
   )
