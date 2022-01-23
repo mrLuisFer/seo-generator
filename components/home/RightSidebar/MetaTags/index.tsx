7b74042ad9a3379dca215e530a17ba06ab00d23f
mrLuisFer <mrluisfeer@gmail.com>
2022-01-23 13:03:30 -0600

    add func to copy text

--- components/home/RightSidebar/MetaTags/index.tsx
+++ components/home/RightSidebar/MetaTags/index.tsx
@@ -1,3 +1,4 @@
+import { MutableRefObject, useRef } from 'react'
 import KeywordsTag from './KeywordsTag'
 import MetaTag from './MetaTag'
 import { useGetInputStates } from 'hooks/useGetInputStates'
@@ -6,17 +7,27 @@ export default function MetaTags() {
   const { descriptionTextarea, keywordsInput, ogTypeInput, titleInput, author, copyright, ogUrl, rating, robots } =
     useGetInputStates()
 
+  const codeTagRef: MutableRefObject<HTMLElement> = useRef()
+
+  const handleCopyText = () => {
+    const nodeValue: string = codeTagRef.current.innerText
+    navigator.clipboard.writeText(nodeValue)
+  }
+
   return (
-    <code>
-      <MetaTag tagTitle="Title" htmlTemplate={`<title>${titleInput}</title>`} />
-      <MetaTag tagTitle="Description" htmlTemplate={`<meta name="description" content="${descriptionTextarea}" />`} />
-      <KeywordsTag tagValue={keywordsInput} />
-      <MetaTag tagTitle="Og:Type" htmlTemplate={`<meta property=”og:type” content="${ogTypeInput}" />`} />
-      <MetaTag tagTitle="Author" htmlTemplate={`<meta name="author" content="${author}">`} />
-      <MetaTag tagTitle="Copyright" htmlTemplate={`<meta name="dcterms.rightsHolder" content="${copyright}">`} />
-      <MetaTag tagTitle="OgUrl" htmlTemplate={`<meta property="og:url" content="${ogUrl}"/>`} />
-      <MetaTag tagTitle="Rating" htmlTemplate={`<meta name="rating" content="${rating}">`} />
-      <MetaTag tagTitle="Robots" htmlTemplate={`<meta name="robots" content="${robots}" />`} />
-    </code>
+    <>
+      <button onClick={handleCopyText}>copy</button>
+      <code ref={codeTagRef}>
+        <MetaTag tagTitle="Title" htmlTemplate={`<title>${titleInput}</title>`} />
+        <MetaTag tagTitle="Description" htmlTemplate={`<meta name="description" content="${descriptionTextarea}" />`} />
+        <KeywordsTag tagValue={keywordsInput} />
+        <MetaTag tagTitle="Og:Type" htmlTemplate={`<meta property=”og:type” content="${ogTypeInput}" />`} />
+        <MetaTag tagTitle="Author" htmlTemplate={`<meta name="author" content="${author}">`} />
+        <MetaTag tagTitle="Copyright" htmlTemplate={`<meta name="dcterms.rightsHolder" content="${copyright}">`} />
+        <MetaTag tagTitle="OgUrl" htmlTemplate={`<meta property="og:url" content="${ogUrl}"/>`} />
+        <MetaTag tagTitle="Rating" htmlTemplate={`<meta name="rating" content="${rating}">`} />
+        <MetaTag tagTitle="Robots" htmlTemplate={`<meta name="robots" content="${robots}" />`} />
+      </code>
+    </>
   )
 }
