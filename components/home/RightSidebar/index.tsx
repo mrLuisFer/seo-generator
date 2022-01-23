d4b6821369907f48cd0f0a9f9b1aa711b2b48ed5
mrLuisFer <mrluisfeer@gmail.com>
2022-01-23 12:14:29 -0600

    remove unecessary hook

--- components/home/RightSidebar/index.tsx
+++ components/home/RightSidebar/index.tsx
@@ -2,13 +2,9 @@ import { Box } from '@chakra-ui/react'
 import { gray } from 'utils/chakraColors'
 import MetaTags from './MetaTags'
 import HeaderSidebar from './HeaderSidebar'
-import SidebarTitlesCtxProvider, { useSidebarTitlesCtx } from 'context/SidebarTitlesCtx'
+import SidebarTitlesCtxProvider from 'context/SidebarTitlesCtx'
 
 export default function RightSidebar() {
-  const { showTitles } = useSidebarTitlesCtx()
-  console.log(showTitles)
-
-  // TODO: add fixed position for the sidebar
   return (
     <SidebarTitlesCtxProvider>
       <Box
@@ -19,7 +15,6 @@ export default function RightSidebar() {
         gridColumn="span 2"
         borderRadius="15px"
         p="1.5rem"
-        overflowX="auto"
         boxShadow="md"
       >
         <HeaderSidebar />
