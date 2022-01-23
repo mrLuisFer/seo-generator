13649977aa912e9fa5a4ef8266a3f600fb173c5a
mrLuisFer <mrluisfeer@gmail.com>
2022-01-23 11:56:53 -0600

    remove states in the Home component

--- components/home/index.tsx
+++ components/home/index.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { Box } from '@chakra-ui/react'
 import { useSidebarCtx } from 'hooks/useSidebarCtx'
 import ShowSidebarBtn from './RightSidebar/HeaderSidebar/HidesidebarBtn/ShowSidebarBtn'
@@ -6,50 +5,13 @@ import LeftSidebar from './LeftSidebar'
 import RightSidebar from './RightSidebar'
 
 export default function Home() {
-  // Required Inputs
-  const [descriptionTextarea, setDescriptionTextarea] = useState('')
-  const [keywordsInput, setKeywordsInput] = useState('')
-  const [ogTypeInput, setOgTypeInput] = useState('')
-  const [titleInput, setTitleInput] = useState('')
-
-  // Optional Inputs
-  const [author, setAuthor] = useState('')
-  const [copyright, setCopyright] = useState('')
-  const [ogUrl, setOgUrl] = useState('')
-  const [rating, setRating] = useState('')
-  const [robots, setRobots] = useState('')
-
   const { hideSidebar } = useSidebarCtx()
   const gridStyle = hideSidebar ? '1fr 1fr 1fr 50px' : '1fr 1fr 1fr 1fr 1fr'
 
   return (
     <Box display="grid" gridTemplateColumns={gridStyle} gridGap="2rem">
-      <LeftSidebar
-        setDescriptionTextarea={setDescriptionTextarea}
-        setKeywordsInput={setKeywordsInput}
-        setOgTypeInput={setOgTypeInput}
-        setTitleInput={setTitleInput}
-        setAuthor={setAuthor}
-        setCopyright={setCopyright}
-        setOgUrl={setOgUrl}
-        setRating={setRating}
-        setRobots={setRobots}
-      />
-      {!hideSidebar ? (
-        <RightSidebar
-          descriptionTextarea={descriptionTextarea}
-          keywordsInput={keywordsInput}
-          ogTypeInput={ogTypeInput}
-          titleInput={titleInput}
-          author={author}
-          copyright={copyright}
-          ogUrl={ogUrl}
-          rating={rating}
-          robots={robots}
-        />
-      ) : (
-        <ShowSidebarBtn />
-      )}
+      <LeftSidebar />
+      {!hideSidebar ? <RightSidebar /> : <ShowSidebarBtn />}
     </Box>
   )
 }
