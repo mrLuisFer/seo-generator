07fb0781a2c979ff6de2d429165ac0429d929239
mrLuisFer <mrluisfeer@gmail.com>
2022-01-26 17:12:21 -0600

    remove static ref for a forwardref component

--- components/home/RightSidebar/MetaTags/index.tsx
+++ components/home/RightSidebar/MetaTags/index.tsx
@@ -1,33 +1,26 @@
-import { MutableRefObject, useRef } from 'react'
+import * as React from 'react'
 import KeywordsTag from './KeywordsTag'
 import MetaTag from './MetaTag'
 import { useGetInputStates } from 'hooks/useGetInputStates'
 
-export default function MetaTags() {
+// eslint-disable-next-line react/display-name
+const MetaTags = React.forwardRef<HTMLElement, any>((_, ref) => {
   const { descriptionTextarea, keywordsInput, ogTypeInput, titleInput, author, copyright, ogUrl, rating, robots } =
     useGetInputStates()
 
-  const codeTagRef: MutableRefObject<HTMLElement> = useRef()
-
-  const handleCopyText = () => {
-    const nodeValue: string = codeTagRef.current.innerText
-    navigator.clipboard.writeText(nodeValue)
-  }
-
   return (
-    <>
-      <button onClick={handleCopyText}>copy</button>
-      <code ref={codeTagRef}>
-        <MetaTag tagTitle="Title" htmlTemplate={`<title>${titleInput}</title>`} />
-        <MetaTag tagTitle="Description" htmlTemplate={`<meta name="description" content="${descriptionTextarea}" />`} />
-        <KeywordsTag tagValue={keywordsInput} />
-        <MetaTag tagTitle="Og:Type" htmlTemplate={`<meta property=”og:type” content="${ogTypeInput}" />`} />
-        <MetaTag tagTitle="Author" htmlTemplate={`<meta name="author" content="${author}">`} />
-        <MetaTag tagTitle="Copyright" htmlTemplate={`<meta name="dcterms.rightsHolder" content="${copyright}">`} />
-        <MetaTag tagTitle="OgUrl" htmlTemplate={`<meta property="og:url" content="${ogUrl}"/>`} />
-        <MetaTag tagTitle="Rating" htmlTemplate={`<meta name="rating" content="${rating}">`} />
-        <MetaTag tagTitle="Robots" htmlTemplate={`<meta name="robots" content="${robots}" />`} />
-      </code>
-    </>
+    <code ref={ref}>
+      <MetaTag tagTitle="Title" htmlTemplate={`<title>${titleInput}</title>`} />
+      <MetaTag tagTitle="Description" htmlTemplate={`<meta name="description" content="${descriptionTextarea}" />`} />
+      <KeywordsTag tagValue={keywordsInput} />
+      <MetaTag tagTitle="Og:Type" htmlTemplate={`<meta property=”og:type” content="${ogTypeInput}" />`} />
+      <MetaTag tagTitle="Author" htmlTemplate={`<meta name="author" content="${author}">`} />
+      <MetaTag tagTitle="Copyright" htmlTemplate={`<meta name="dcterms.rightsHolder" content="${copyright}">`} />
+      <MetaTag tagTitle="OgUrl" htmlTemplate={`<meta property="og:url" content="${ogUrl}"/>`} />
+      <MetaTag tagTitle="Rating" htmlTemplate={`<meta name="rating" content="${rating}">`} />
+      <MetaTag tagTitle="Robots" htmlTemplate={`<meta name="robots" content="${robots}" />`} />
+    </code>
   )
-}
+})
+
+export default MetaTags
