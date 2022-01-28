645a72fbe86d7fb7ac2b0ff3c28ea1deb3407b1e
mrLuisFer <mrluisfeer@gmail.com>
2022-01-27 22:45:42 -0600

    fix darkmode styles for mobile menu

--- components/MenuMobile/index.tsx
+++ components/MenuMobile/index.tsx
@@ -4,9 +4,12 @@ import { useMobileMenuCtx } from 'hooks/useMobileMenuCtx'
 import { gray, blue } from 'utils/chakraColors'
 import HoverPopup from 'lib/HoverPopup'
 import CommonBtn from 'lib/CommonBtn'
+import { useDarkMode } from 'hooks/useDarkMode'
 
 export default function MenuMobile() {
   const { isOpen, setIsOpen } = useMobileMenuCtx()
+
+  const { isDarkMode } = useDarkMode()
 
   const handleCloseMenu = () => {
     setIsOpen(false)
@@ -22,15 +25,15 @@ export default function MenuMobile() {
         p="1rem"
         position="fixed"
         right="0"
+        shadow="-3px 1px 9px rgba(0,0,0,0.3)"
         top="0"
         w={{ base: '100%', md: '20rem' }}
-        shadow="-3px 1px 9px rgba(0,0,0,0.3)"
       >
         <Flex align="center" justify="space-between">
-          <Heading as="h2" fontSize="md">
+          <Heading as="h2" fontSize="md" color={gray.$100}>
             Menu
           </Heading>
-          <HoverPopup text="Close" bgColor={gray.$900}>
+          <HoverPopup text="Close" bgColor={isDarkMode ? gray.$200 : gray.$700}>
             <CommonBtn hoverProps={{ color: blue.$600 }} onClick={handleCloseMenu}>
               <MdOutlineClose size="1.8rem" />
             </CommonBtn>
