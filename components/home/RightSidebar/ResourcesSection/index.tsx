82a37b46b937503e7a2256aa11045ebae3184e32
mrLuisFer <mrluisfeer@gmail.com>
2022-01-27 22:45:10 -0600

    add darkmode styles for resources section

--- components/home/RightSidebar/ResourcesSection/index.tsx
+++ components/home/RightSidebar/ResourcesSection/index.tsx
@@ -1,21 +1,26 @@
 import { Box, Fade } from '@chakra-ui/react'
 import { useSidebarTitlesCtx } from 'context/SidebarTitlesCtx'
 import { gray } from 'utils/chakraColors'
+import { useDarkMode } from 'hooks/useDarkMode'
 
 export default function ResourcesSection() {
   const { showTitles } = useSidebarTitlesCtx()
+  const { isDarkMode } = useDarkMode()
 
   const commonRadiusAndPadding: string = '1.5rem'
 
   return (
-    <>
-      <Fade in={!showTitles}>
-        {!showTitles && (
-          <Box bg={gray.$600} borderRadius={commonRadiusAndPadding} p={commonRadiusAndPadding}>
-            <p>some</p>
-          </Box>
-        )}
-      </Fade>
-    </>
+    <Fade in={!showTitles}>
+      {!showTitles && (
+        <Box
+          bg={isDarkMode ? gray.$600 : gray.$400}
+          color={isDarkMode ? gray.$100 : gray.$800}
+          borderRadius={commonRadiusAndPadding}
+          p={commonRadiusAndPadding}
+        >
+          <p>some</p>
+        </Box>
+      )}
+    </Fade>
   )
 }
