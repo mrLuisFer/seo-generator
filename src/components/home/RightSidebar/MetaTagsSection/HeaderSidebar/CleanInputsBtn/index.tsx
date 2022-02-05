6100fb5a89d5d5035cf4eeb0aecbbcff529ff42f
mrLuisFer <mrluisfeer@gmail.com>
2022-02-04 21:45:40 -0600

    add Cleaner for clear input values

---
+++ src/components/home/RightSidebar/MetaTagsSection/HeaderSidebar/CleanInputsBtn/index.tsx
@@ -0,0 +1,21 @@
+import { MdDeleteOutline } from 'react-icons/md'
+import HeaderBtn from '../HeaderBtn'
+import { useGetInputStates } from 'hooks/useGetInputStates'
+import HoverPopup from 'lib/HoverPopup'
+
+export default function CleanInputsBtn() {
+  const { setTitleInput, setDescriptionTextarea } = useGetInputStates()
+
+  const handleClearInputs = () => {
+    setTitleInput('')
+    setDescriptionTextarea('')
+  }
+
+  return (
+    <HeaderBtn onClick={handleClearInputs}>
+      <HoverPopup text="Clear inputs" width="90px">
+        <MdDeleteOutline size="1.5rem" />
+      </HoverPopup>
+    </HeaderBtn>
+  )
+}
--- src/components/home/RightSidebar/MetaTagsSection/HeaderSidebar/index.tsx
+++ src/components/home/RightSidebar/MetaTagsSection/HeaderSidebar/index.tsx
@@ -2,6 +2,7 @@ import { Flex, HStack } from '@chakra-ui/react'
 import HidesidebarBtn from './HidesidebarBtn'
 import HideTitleBtn from './HideTitlesBtn'
 import CopyTextBtn from './CopyTextBtn'
+import CleanInputsBtn from './CleanInputsBtn'
 
 interface HeaderSidebarProps {
   copyFunc: () => void
@@ -10,7 +11,8 @@ interface HeaderSidebarProps {
 // eslint-disable-next-line react/display-name
 const HeaderSidebar = ({ copyFunc }: HeaderSidebarProps) => {
   return (
-    <Flex justify="flex-end">
+    <Flex justify="space-between">
+    <CleanInputsBtn />
       <HStack alignItems="center" spacing="1.5rem">
         <CopyTextBtn clickFunc={copyFunc} />
         <HideTitleBtn />
