86f894ecbb002928531c894508cf758b83d0c8ae
mrLuisFer <mrluisfeer@gmail.com>
2022-02-04 20:12:52 -0600

    add value prop for metatag for shows the metatag

--- src/components/home/RightSidebar/MetaTagsSection/MetaTags/MetaTag.tsx
+++ src/components/home/RightSidebar/MetaTagsSection/MetaTags/MetaTag.tsx
@@ -4,13 +4,19 @@ import SidebarTagTitle from '../SidebarTagTitle'
 interface MetaTagProps {
   tagTitle: string
   htmlTemplate: string
+  value: string
 }
 
-export default function MetaTag({ tagTitle, htmlTemplate }: MetaTagProps) {
+export default function MetaTag({ tagTitle, htmlTemplate, value }: MetaTagProps) {
+  const validateValueLength: boolean = value.length < 5
   return (
     <>
-      <SidebarTagTitle>{tagTitle}</SidebarTagTitle>
-      <Highlight>{htmlTemplate}</Highlight>
+      {!validateValueLength && (
+        <>
+          <SidebarTagTitle>{tagTitle}</SidebarTagTitle>
+          <Highlight>{htmlTemplate}</Highlight>
+        </>
+      )}
     </>
   )
 }
