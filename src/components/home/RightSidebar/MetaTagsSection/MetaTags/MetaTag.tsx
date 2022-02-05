8dc767fa4f2b69d7c7a51d56d1979425207d1260
mrLuisFer <mrluisfeer@gmail.com>
2022-02-04 22:21:40 -0600

    remove value for show metatags

--- src/components/home/RightSidebar/MetaTagsSection/MetaTags/MetaTag.tsx
+++ src/components/home/RightSidebar/MetaTagsSection/MetaTags/MetaTag.tsx
@@ -4,19 +4,13 @@ import SidebarTagTitle from '../SidebarTagTitle'
 interface MetaTagProps {
   tagTitle: string
   htmlTemplate: string
-  value: string
 }
 
-export default function MetaTag({ tagTitle, htmlTemplate, value }: MetaTagProps) {
-  const validateValueLength: boolean = value.length < 5
+export default function MetaTag({ tagTitle, htmlTemplate }: MetaTagProps) {
   return (
     <>
-      {!validateValueLength && (
-        <>
-          <SidebarTagTitle>{tagTitle}</SidebarTagTitle>
-          <Highlight>{htmlTemplate}</Highlight>
-        </>
-      )}
+      <SidebarTagTitle>{tagTitle}</SidebarTagTitle>
+      <Highlight>{htmlTemplate}</Highlight>
     </>
   )
 }
--- src/components/home/RightSidebar/MetaTagsSection/MetaTags/index.tsx
+++ src/components/home/RightSidebar/MetaTagsSection/MetaTags/index.tsx
@@ -8,13 +8,13 @@ const MetaTags = forwardRef<HTMLElement, any>((_, ref) => {
 
   return (
     <code ref={ref}>
-      <MetaTag tagTitle="Title" htmlTemplate={`<title>${titleInput}</title>`} value={titleInput} />
-      <MetaTag tagTitle="Description" htmlTemplate={`<meta name="description" content="${descriptionTextarea}" />`} value={descriptionTextarea} />
-      <MetaTag tagTitle="Og:Type" htmlTemplate={`<meta property=”og:type” content="${ogTypeInput}" />`} value={ogTypeInput} />
-      <MetaTag tagTitle="Author" htmlTemplate={`<meta name="author" content="${author}">`} value={author} />
-      <MetaTag tagTitle="OgUrl" htmlTemplate={`<meta property="og:url" content="${ogUrl}"/>`} value={ogUrl} />
-      <MetaTag tagTitle="Rating" htmlTemplate={`<meta name="rating" content="${rating}">`} value={rating} />
-      <MetaTag tagTitle="Robots" htmlTemplate={`<meta name="robots" content="${robots}" />`} value={robots} />
+      <MetaTag tagTitle="Title" htmlTemplate={`<title>${titleInput}</title>`} />
+      <MetaTag tagTitle="Description" htmlTemplate={`<meta name="description" content="${descriptionTextarea}" />`} />
+      <MetaTag tagTitle="Og:Type" htmlTemplate={`<meta property=”og:type” content="${ogTypeInput}" />`} />
+      <MetaTag tagTitle="Author" htmlTemplate={`<meta name="author" content="${author}">`} />
+      <MetaTag tagTitle="OgUrl" htmlTemplate={`<meta property="og:url" content="${ogUrl}"/>`} />
+      <MetaTag tagTitle="Rating" htmlTemplate={`<meta name="rating" content="${rating}">`} />
+      <MetaTag tagTitle="Robots" htmlTemplate={`<meta name="robots" content="${robots}" />`} />
     </code>
   )
 })
