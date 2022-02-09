eb82c182048a6f913391d033b884a61dd5544189
mrLuisFer <mrluisfeer@gmail.com>
2022-02-09 11:14:22 -0600

    remove contributors component

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,6 +1,5 @@
 import { Flex, HStack } from '@chakra-ui/react'
 import MenuIcon from './MenuIcon'
-// import Contributors from './Contributors'
 import DarkModeToggle from './DarkModeToggle'
 import Logo from './Logo'
 
@@ -10,7 +9,6 @@ export default function Header() {
       <Logo />
       <Flex justifyContent="space-between" alignItems="center" gridGap="1.5rem">
         <DarkModeToggle />
-        {/*<Contributors />*/}
         <MenuIcon />
       </Flex>
     </HStack>
