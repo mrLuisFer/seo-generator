5e434c2508adeb6fd6f82151ee44d76ac929acec
mrLuisFer <mrluisfeer@gmail.com>
2022-03-19 13:17:51 -0600

    create and interface array to iterate

---
+++ src/components/home/RightSidebar/ResourcesSection/resources.ts
@@ -0,0 +1,20 @@
+interface IResource {
+  url: string
+  title: string
+  shortDescription?: string
+}
+
+const resources: Array<IResource> = [
+  {
+    url: '',
+    title: '',
+    shortDescription: '',
+  },
+  {
+    url: '',
+    title: '',
+    shortDescription: '',
+  },
+]
+
+export default resources
