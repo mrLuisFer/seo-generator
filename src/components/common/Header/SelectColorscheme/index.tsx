a407bc2da19857f9e3954ed057b98384d1892deb
Luis Alvarez <mrluisfeer@gmail.com>
2022-04-21 23:32:53 -0500

    add HoverPopup for select element

--- src/components/common/Header/SelectColorscheme/index.tsx
+++ src/components/common/Header/SelectColorscheme/index.tsx
@@ -1,13 +1,17 @@
+import { useRef } from "react"
 import { Flex } from '@chakra-ui/react'
 import { useColorschemeCtx } from 'context/HighlightColorschemeCtx'
 import { AiOutlineBgColors } from 'react-icons/ai'
 import Select from 'react-select'
 import themes from './themes'
 import { useDarkMode } from 'hooks/useDarkMode'
+import HoverPopup from 'lib/HoverPopup'
+
 
 export default function SelectColorscheme() {
   const { setColorscheme } = useColorschemeCtx()
   const { isDarkMode } = useDarkMode()
+  const selectRef = useRef(null)
 
   const bg = !isDarkMode ? '#F7FAFC' : '#1A202C'
   const txtColor = isDarkMode ? '#ffffff' : '#000000'
@@ -33,10 +37,13 @@ export default function SelectColorscheme() {
 
   return (
     <Flex alignItems="center" gridGap="1rem">
-      <label htmlFor="select-theme">
-        <AiOutlineBgColors size="1.5rem" />
+      <label htmlFor="select-theme" onClick={() => { selectRef.current.focus() }}>
+        <HoverPopup text="Select Syntax Theme" width="100px">
+          <AiOutlineBgColors size="1.5rem" />
+        </HoverPopup>
       </label>
       <Select
+        ref={selectRef}
         name="select-theme"
         styles={customStyles}
         id="select-theme"
