670e9810141dbd02e26082d5487cff4aa4db464d
mrLuisFer <mrluisfeer@gmail.com>
2022-04-21 20:17:08 -0500

    fix react select styles in the darkmode

--- src/components/common/Header/SelectColorscheme/index.tsx
+++ src/components/common/Header/SelectColorscheme/index.tsx
@@ -3,18 +3,46 @@ import { useColorschemeCtx } from 'context/HighlightColorschemeCtx'
 import { AiOutlineBgColors } from 'react-icons/ai'
 import Select from 'react-select'
 import themes from './themes'
+import { useDarkMode } from 'hooks/useDarkMode'
 
 export default function SelectColorscheme() {
   const { setColorscheme } = useColorschemeCtx()
+  const { isDarkMode } = useDarkMode()
 
-  const handleSelect = (e: any) => {
-    setColorscheme(e)
+  const bg = !isDarkMode ? '#F7FAFC' : '#1A202C'
+  const txtColor = isDarkMode ? '#ffffff' : '#000000'
+  const customStyles = {
+    option: (provided) => ({
+      ...provided,
+      color: '#000',
+    }),
+    control: (provided) => ({
+      ...provided,
+      background: bg,
+      color: txtColor,
+    }),
+    container: (provided) => ({
+      ...provided,
+      background: bg,
+    }),
+    singleValue: (provided) => ({
+      ...provided,
+      color: txtColor
+    })
   }
 
   return (
     <Flex alignItems="center" gridGap="1rem">
-      <AiOutlineBgColors size="1.5rem" />
-      <Select options={themes} onChange={(e) => handleSelect(e)} />
+      <label htmlFor="select-theme">
+        <AiOutlineBgColors size="1.5rem" />
+      </label>
+      <Select
+        name="select-theme"
+        styles={customStyles}
+        id="select-theme"
+        options={themes}
+        onChange={(e) => setColorscheme(e)}
+      />
     </Flex>
   )
 }
