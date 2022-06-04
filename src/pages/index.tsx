b1d219a0179f3ad365f651c834335c34af82eb4f
Luis Alvarez <mrluisfeer@gmail.com>
2022-06-04 17:49:40 -0500

    remove Home folder but move the components one top level

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,21 @@
 import SEO from 'components/common/SEO'
-import Home from 'components/home'
+import { Box } from '@chakra-ui/react'
+import { useSidebarCtx } from 'hooks/useSidebarCtx'
+import ShowSidebarBtn from 'components/RightSidebar/MetaTagsSection/HeaderSidebar/HidesidebarBtn/ShowSidebarBtn'
+import LeftSidebar from 'components/LeftSidebar'
+import RightSidebar from 'components/RightSidebar'
 
 export default function Main() {
+  const { hideSidebar } = useSidebarCtx()
+  const gridStyle = hideSidebar ? '1fr 1fr 1fr 100px' : '1fr 1fr 1fr 1fr'
+
   return (
     <>
       <SEO title="Home" description="The easiest way to create the important meta tags for your web page" />
-      <Home />
+      <Box display="grid" gridTemplateColumns={gridStyle} gridGap="2rem">
+        <LeftSidebar />
+        {hideSidebar ? <ShowSidebarBtn /> : <RightSidebar />}
+      </Box>
     </>
   )
 }
