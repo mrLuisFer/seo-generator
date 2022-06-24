b6a59e509b2a0cf4d88f04047251e1fc68a1a544
mrLuisFer <mrluisfeer@gmail.com>
2022-06-24 01:28:15 -0500

    fix errors on the menu mobile

--- src/components/MenuMobile/index.tsx
+++ src/components/MenuMobile/index.tsx
@@ -1,31 +1,30 @@
-import { Dispatch, SetStateAction } from 'react'
-import { Box, Slide, Flex, Heading, Text } from '@chakra-ui/react'
-import { MdOutlineClose } from 'react-icons/md'
-import { gray, blue, white } from 'utils/chakraColors'
-import HoverPopup from 'lib/HoverPopup'
-import CommonBtn from 'lib/CommonBtn'
-import { useDarkMode } from 'hooks/useDarkMode'
-import DarkModeToggle from 'components/common/Header/DarkModeToggle'
-import SelectColorscheme from 'components/common/Header/SelectColorscheme'
+import { Dispatch, SetStateAction } from 'react';
+import { Box, Slide, Flex, Heading, Text } from '@chakra-ui/react';
+import { MdOutlineClose } from 'react-icons/md';
+import HoverPopup from 'lib/HoverPopup';
+import CommonBtn from 'lib/CommonBtn';
+import { useDarkMode } from 'hooks/useDarkMode';
+import DarkModeToggle from 'components/common/Header/DarkModeToggle';
+import SelectColorscheme from 'components/common/Header/SelectColorscheme';
 
 interface MenuMobileProps {
-  isOpen: boolean
-  setIsOpen: Dispatch<SetStateAction<boolean>>
+  isOpen: boolean;
+  setIsOpen: Dispatch<SetStateAction<boolean>>;
 }
 
 export default function MenuMobile({ isOpen, setIsOpen }: MenuMobileProps) {
-  const { isDarkMode } = useDarkMode()
+  const { isDarkMode } = useDarkMode();
 
   const handleCloseMenu = () => {
-    setIsOpen(false)
-  }
+    setIsOpen(false);
+  };
 
   return (
     <Box position="absolute" right={0} top={0} zIndex="15">
       <Slide direction="right" in={isOpen}>
         <Box
           as="aside"
-          bg={gray.$800}
+          bg="gray.800"
           borderRadius={{ base: '0', md: '10px 0 0 10px' }}
           minH="100vh"
           zIndex="20"
@@ -37,11 +36,11 @@ export default function MenuMobile({ isOpen, setIsOpen }: MenuMobileProps) {
           w={{ base: '100%', md: '20rem' }}
         >
           <Flex align="center" justify="space-between">
-            <Heading as="h2" fontSize="md" color={gray.$100}>
+            <Heading as="h2" fontSize="md" color="gray.100">
               Menu
             </Heading>
-            <HoverPopup text="Close" bgColor={isDarkMode ? gray.$200 : gray.$700}>
-              <CommonBtn hoverProps={{ color: blue.$600 }} onClick={handleCloseMenu}>
+            <HoverPopup text="Close" width="auto" bgColor={isDarkMode ? 'gray.200' : 'gray.700'}>
+              <CommonBtn hoverProps={{ color: 'blue.600' }} onClick={handleCloseMenu}>
                 <MdOutlineClose size="1.8rem" />
               </CommonBtn>
             </HoverPopup>
@@ -53,20 +52,20 @@ export default function MenuMobile({ isOpen, setIsOpen }: MenuMobileProps) {
         </Box>
       </Slide>
     </Box>
-  )
+  );
 }
 
 type MenuSlotProps = {
-  textSlot: string
-  componentOrIcon: JSX.Element
-}
+  textSlot: string;
+  componentOrIcon: JSX.Element;
+};
 function MenuSlot({ textSlot, componentOrIcon }: MenuSlotProps) {
   return (
     <Flex alignItems="center" gridGap="1rem" mt="1.5rem">
       {componentOrIcon}
-      <Text color={white} fontWeight="semibold" fontSize="1rem">
+      <Text color="white" fontWeight="semibold" fontSize="1rem">
         {textSlot}
       </Text>
     </Flex>
-  )
+  );
 }
